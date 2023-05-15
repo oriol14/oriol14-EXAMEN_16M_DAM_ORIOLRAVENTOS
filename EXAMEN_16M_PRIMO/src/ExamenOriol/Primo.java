bac6f5e90727dc3203295ccb1cc4487261ed6f81
oriol <oriol@DESKTOP-HC4FTOP>
2023-05-15 09:20:54 +0200

    HECHO POR ORIOL

---
+++ EXAMEN_16M_PRIMO/src/ExamenOriol/Primo.java
@@ -0,0 +1,20 @@
+package ExamenOriol;
+
+import java.util.Scanner;
+
+public class Primo {
+	
+	public static void main(String[]arg) {
+		
+		Scanner ent=new Scanner(System.in);
+		int numPrimo;
+		
+		System.out.println("Buenos días Ceinmark, soy Marta Gómez\r\n"
+				+ "Introduce un número para averiguar si es primo o no: ");
+		
+		
+		numPrimo=ent.nextInt();
+	
+		
+	}
+}
